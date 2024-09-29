886878019b678afc5ceb796a952841346e14d05b
poliqmi231 <NE0Nbr0@yandex.ru>
2024-09-29 09:04:37 +0300

    last_commit

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -1,10 +1,17 @@
 from fastapi import APIRouter, UploadFile, File, HTTPException
+from fastapi.responses import StreamingResponse
 from uuid import uuid4
 from app.database import VideoStateRepository, VideoDataRepository
 from app.database.mongo import MongoDBClient
 from app.config import DATABASE_URL, UPLOAD_PATH
 from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure
+import pandas as pd
+import io
+import json
+import os #dlya test ruchki
+
+
 
 router = APIRouter()
 
@@ -12,6 +19,7 @@ router = APIRouter()
 db_client = MongoDBClient(uri=DATABASE_URL, db_name="video_db")
 video_state_repo = VideoStateRepository(db_client)
 video_data_repo = VideoDataRepository(db_client)
+
 
 
 @router.post("/upload-video/")
@@ -65,3 +73,66 @@ async def get_video_status(vid: str):
         raise HTTPException(status_code=404, detail="Видео не найдено.")
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Ошибка получения статуса видео: {str(e)}")
+
+
+
+'''TODO
+@router.post("/test_json_output/")
+async def test_json_to_csv(file: UploadFile = File(...)):
+    # Проверка на формат
+    if not file.filename.endswith('.json'):
+        raise HTTPException(status_code=400, detail="Файл должен быть JSON")
+
+    contents = await file.read()
+    
+    try:
+        # Загружаем JSON данные
+        json_data = json.loads(contents)
+        
+        # Извлекаем первый словарь
+        #first_dict = next(iter(json_data.values()))
+        # Преобразуем JSON в DataFrame
+        audio_df = pd.DataFrame(json_data['audio'])
+        spekears_df = pd.DataFrame(json_data['spekears'])
+        video_df = pd.DataFrame(json_data['video'])
+
+        # Объединяем все DataFrame
+        #combined_df = pd.concat([audio_df, spekears_df, video_df], axis=1)
+
+        # Сохраняем в CSV
+        audio_df.to_csv('output1.csv', index=False)
+        spekears_df.to_csv('output2.csv', index=False)
+        video_df.to_csv('output3.csv', index=False)
+        temp_list = ['audio_df.csv', 'spekears_df.csv', 'video_df.csv']
+        temp_dict = {"0": "", "1": "", "2": ""}
+
+        
+    except (ValueError, StopIteration) as e:
+        raise HTTPException(status_code=400, detail="Неверный формат JSON или файл пуст")
+
+    for i in range(3):
+    # Преобразуем DataFrame в CSV
+        csv_buffer = io.StringIO()
+        data.to_csv(csv_buffer, index=False)
+        csv_buffer.seek(0)
+
+        # Сохраняем CSV файл на сервере
+        csv_filename = temp_list[i]
+        csv_filepath = os.path.join(UPLOAD_DIR, csv_filename)
+        with open(csv_filepath, 'w') as f:
+            f.write(csv_buffer.getvalue())
+
+        # Возвращаем URL к загруженному CSV файлу
+        file_url = f"http://localhost:8000/{UPLOAD_DIR}/{csv_filename}"
+        temp_dict[str(i)] = file_url
+        print(temp_dict)
+    return temp_dict'''
+
+@router.post("/test_json_output/")
+async def test_json_to_csv(file: UploadFile = File(...)):
+    try:
+        # Можно добавить обработку данных, если нужно
+        return file  # Возвращаем тот же JSON
+        
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
--- backend/app/main.py
+++ backend/app/main.py
@@ -3,10 +3,14 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.api import routes
 from app.database import MongoDBClient
 from .config.config import DATABASE_URL
+from fastapi.staticfiles import StaticFiles
+
+
 
 
 app = FastAPI()
 
+app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 
 # TODO: Change in prod
 app.add_middleware(
