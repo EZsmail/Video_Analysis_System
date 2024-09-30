dc858fac5332cc3d39ca01b32b4a7db2f01a10b1
EZsmail <tolagvozdev@gmail.com>
2024-09-30 23:54:39 +0300

    init 0.0.1

---
+++ backend_golang/cmd/video-analyzer/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"backend-golang/internal/config"
+	"log/slog"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting server", slog.String("env", cfg.Env))
+
+	router := chi.NewRouter()
+
+	router.Use(middleware.RequestID)
+	router.Use(middleware.RealIP)
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
+
+	// TODO: routers
+
+	// TODO: start server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
